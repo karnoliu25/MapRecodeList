efaa753c8492623490fca68d671b6d5dd113f5a2
KarnoLiu <karnoliu@163.com>
2025-04-26 19:29:02 +0800

    初版UI

---
+++ script.js
@@ -0,0 +1,10 @@
+// if (navigator.geolocation) {
+//   navigator.geolocation.getCurrentPosition(
+//     function (pos) {
+//       console.log(pos);
+//     },
+//     function () {
+//       console.log("获取位置失败");
+//     }
+//   );
+// }
